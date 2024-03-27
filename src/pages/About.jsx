c1ee11d492ad3eabfca8f739453f10ee7057fe58
Soo <li_xiuheng@hotmail.com>
2024-03-27 21:24:33 +0000

    change About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,23 +10,6 @@ function About() {
 				bootcamp. We were inspired by our mutual passion for food, and wanted to
 				create an easy way for a home cook to find and compile recipes.
 			</h2>
-
-			<div className="container">
-				<div className="row">
-					<div className="col card text-center">
-						<h3>Soo</h3>
-						<p>Hi! I'm Soo</p>
-					</div>
-					<div className="col card text-center">
-						<h3>Eric</h3>
-						<p>Hi! I'm Eric</p>
-					</div>
-					<div className="col card text-center">
-						<h3>John</h3>
-						<p>Hi! I'm John</p>
-					</div>
-				</div>
-			</div>
 		</div>
 	);
 }
